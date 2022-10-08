3a0158ec85418a71382a83c36ad74c7103874468
Brian Lach <brianlach72@gmail.com>
2022-10-07 23:23:55 -0400

    bring in latest version of GlobPattern from dtool
    allows doing full recursive $[wildcard **]

--- src/globPattern.h
+++ src/globPattern.h
@@ -1,16 +1,15 @@
-// Filename: globPattern.h
-// Created by:  drose (30May00)
-//
-////////////////////////////////////////////////////////////////////
-//
-// PANDA 3D SOFTWARE
-// Copyright (c) Carnegie Mellon University.  All rights reserved.
-//
-// All use of this software is subject to the terms of the revised BSD
-// license.  You should have received a copy of this license along
-// with this source code in a file named "LICENSE."
-//
-////////////////////////////////////////////////////////////////////
+/**
+ * PANDA 3D SOFTWARE
+ * Copyright (c) Carnegie Mellon University.  All rights reserved.
+ *
+ * All use of this software is subject to the terms of the revised BSD
+ * license.  You should have received a copy of this license along
+ * with this source code in a file named "LICENSE."
+ *
+ * @file globPattern.h
+ * @author drose
+ * @date 2000-05-30
+ */
 
 #ifndef GLOBPATTERN_H
 #define GLOBPATTERN_H
@@ -19,24 +18,20 @@
 #include "filename.h"
 #include "vector_string.h"
 
-////////////////////////////////////////////////////////////////////
-//       Class : GlobPattern
-// Description : This class can be used to test for string matches
-//               against standard Unix-shell filename globbing
-//               conventions.  It serves as a portable standin for the
-//               Posix fnmatch() call.
-//
-//               A GlobPattern is given a pattern string, which can
-//               contain operators like *, ?, and [].  Then it can be
-//               tested against any number of candidate strings; for
-//               each candidate, it will indicate whether the string
-//               matches the pattern or not.  It can be used, for
-//               example, to scan a directory for all files matching a
-//               particular pattern.
-////////////////////////////////////////////////////////////////////
-class EXPCL_PANDA GlobPattern {
-public:
-  INLINE GlobPattern(const string &pattern = string());
+/**
+ * This class can be used to test for string matches against standard Unix-
+ * shell filename globbing conventions.  It serves as a portable standin for
+ * the Posix fnmatch() call.
+ *
+ * A GlobPattern is given a pattern string, which can contain operators like
+ * *, ?, and [].  Then it can be tested against any number of candidate
+ * strings; for each candidate, it will indicate whether the string matches
+ * the pattern or not.  It can be used, for example, to scan a directory for
+ * all files matching a particular pattern.
+ */
+class GlobPattern {
+PUBLISHED:
+  INLINE GlobPattern(const std::string &pattern = std::string());
   INLINE GlobPattern(const GlobPattern &copy);
   INLINE void operator = (const GlobPattern &copy);
 
@@ -44,42 +39,44 @@ public:
   INLINE bool operator != (const GlobPattern &other) const;
   INLINE bool operator < (const GlobPattern &other) const;
 
-  INLINE void set_pattern(const string &pattern);
-  INLINE const string &get_pattern() const;
+  INLINE void set_pattern(const std::string &pattern);
+  INLINE const std::string &get_pattern() const;
 
   INLINE void set_case_sensitive(bool case_sensitive);
   INLINE bool get_case_sensitive() const;
 
-  INLINE void set_nomatch_chars(const string &nomatch_chars);
-  INLINE const string &get_nomatch_chars() const;
+  INLINE void set_nomatch_chars(const std::string &nomatch_chars);
+  INLINE const std::string &get_nomatch_chars() const;
 
-  INLINE bool matches(const string &candidate) const;
+  INLINE bool matches(const std::string &candidate) const;
+  bool matches_file(Filename candidate) const;
 
-  INLINE void output(ostream &out) const;
+  INLINE void output(std::ostream &out) const;
 
   bool has_glob_characters() const;
-  string get_const_prefix() const;
-  int match_files(vector_string &results, const Filename &cwd = Filename());
+  std::string get_const_prefix() const;
+  int match_files(vector_string &results, const Filename &cwd = Filename()) const;
 
 private:
-  bool matches_substr(string::const_iterator pi,
-                      string::const_iterator pend,
-                      string::const_iterator ci,
-                      string::const_iterator cend) const;
+  bool matches_substr(std::string::const_iterator pi,
+                      std::string::const_iterator pend,
+                      std::string::const_iterator ci,
+                      std::string::const_iterator cend) const;
 
-  bool matches_set(string::const_iterator &pi,
-                   string::const_iterator pend,
+  bool matches_set(std::string::const_iterator &pi,
+                   std::string::const_iterator pend,
                    char ch) const;
 
-  int r_match_files(const Filename &prefix, const string &suffix,
+  int r_match_files(const Filename &prefix, const std::string &suffix,
                     vector_string &results, const Filename &cwd);
+  bool r_matches_file(const std::string &suffix, const Filename &candidate) const;
 
-  string _pattern;
+  std::string _pattern;
   bool _case_sensitive;
-  string _nomatch_chars;
+  std::string _nomatch_chars;
 };
 
-INLINE ostream &operator << (ostream &out, const GlobPattern &glob) {
+INLINE std::ostream &operator << (std::ostream &out, const GlobPattern &glob) {
   glob.output(out);
   return out;
 }
